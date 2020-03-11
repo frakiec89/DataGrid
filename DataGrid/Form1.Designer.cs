514ac33030ad08b1866d9d1d2972c0e4a7abfa99
РИШАТ Ахтямов <rishat-rus@mail.ru>
2020-03-11 08:39:02 +0400

    Добавьте файлы проекта.

---
+++ DataGrid/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace DataGrid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btGo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(40, 34);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(457, 286);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btGo
+            // 
+            this.btGo.Location = new System.Drawing.Point(572, 57);
+            this.btGo.Name = "btGo";
+            this.btGo.Size = new System.Drawing.Size(75, 23);
+            this.btGo.TabIndex = 1;
+            this.btGo.Text = "start";
+            this.btGo.UseVisualStyleBackColor = true;
+            this.btGo.Click += new System.EventHandler(this.btGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btGo);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btGo;
+    }
+}
+
---
+++ DataGrid/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGrid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btGo_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Add("y", "y");
+            dataGridView1.Columns.Add("x", "x");
+            dataGridView1.Columns.Add("step", "шаг");
+
+            var vs = GetData(10 , -1 ,1);
+
+            foreach ( var  item in  vs)
+            {
+                dataGridView1.Rows.Add(item.Y, item.X, item.Step);
+            }
+
+        }
+
+        private List <Masssiv> GetData (double step , double  xIn , double xEnd)
+        {
+            step = (xEnd - xIn) / step;
+
+            List<Masssiv> vs = new List<Masssiv>();
+
+            for  (double i = xIn; i<= xEnd; i+=step  )
+            {
+                vs.Add(new Masssiv { Y=i * i, X=i, Step=step   }); 
+            }
+            return vs;
+        }
+    }
+
+    public  struct Masssiv
+    {
+        public double Y { get; set; }
+        public double X { get; set; }
+        public double Step { get; set; }
+    }
+}
